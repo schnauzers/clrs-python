5b73af47f1aa920e79fbb318aadc4aaba7c53cbb
root <root@localhost.localdomain>
2019-11-28 19:46:23 +0800

    update

--- sort/merge-sort.py
+++ sort/merge-sort.py
@@ -1,11 +1,12 @@
+# merge sort
+
+
 # assume that A is an array
 # the goal is to sort an child sequnce of A(by ascending order)
 # p is the start element index of the child sequnce
 # r is the last element index of the child sequnce
 # elements [p, q] is a sorted child sequence
 # elements [q+1, r] is also a sorted child sequence
-
-
 def merge(A, p, q, r):
     L = A[p:q+1]
     R = A[q+1:r+1]
@@ -30,8 +31,13 @@ def merge(A, p, q, r):
         print("round ", idxA, "of sorting result: ", A)
 
 
+# recursion process
+# A is the array to be sorted
+# p is the index of the start element
+# r is the index of the end element
 def mergeSort(A, p, r):
     if p < r:
+        # try to split equally
         q = (p + r)//2
         mergeSort(A, p, q)
         mergeSort(A, q+1, r)
@@ -39,8 +45,11 @@ def mergeSort(A, p, r):
 
 
 if __name__ == '__main__':
-    A = [2, 4, 5, 7, 1, 2, 3, 6]
+    print("--->Test function merge()...")
+    A = [2, 4, 5, 7, 9, 1, 2, 3, 6]
     print("original seq: ", A)
-    # merge(A, 0, 3, 7)
+    merge(A, 0, 4, 8)
+
+    print("\n--->Test function mergeSort()...")
     B = [1, 3, 7, 2, 4, 9, 10, 1, 11, 12, 18, 9]
     mergeSort(B, 0, len(B)-1)
